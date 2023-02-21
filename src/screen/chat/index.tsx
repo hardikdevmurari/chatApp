739c7e72feb42dc98a93ce04bc8391fe9bc41582
hardikdevmurari <hardikdevmurari1996@gmail.com>
2023-02-21 21:00:31 +0530

    text input is done

--- src/screen/chat/index.tsx
+++ src/screen/chat/index.tsx
@@ -10,6 +10,7 @@ import {
   StatusBar,
   FlatList,
   TouchableWithoutFeedback,
+  TextInput,
 } from 'react-native';
 import chatImage from '../../assets/chat.png';
 import Icon from 'react-native-vector-icons/FontAwesome';
@@ -49,7 +50,29 @@ const ChatScreen: React.FC<Props> = ({navigation, route}) => {
           </View>
         </View>
       </View>
-      <View style={styles.contentArea} />
+      <View style={styles.contentArea}>
+        <View style={styles.chatConversationContainer}>
+          <Text>no message found</Text>
+        </View>
+        <View style={styles.sendMessageContainer}>
+          <View style={styles.customInputBox}>
+            <View style={{margin:10}}>
+              <Icon name={'paperclip'} size={24} color="black" />
+            </View>
+            <TextInput
+              style={{flex: 4,height:100}}
+              placeholder="Write a message"
+              placeholderTextColor={'black'}
+            />
+            <View style={{marginHorizontal:5}}>
+              <Icon name={'microphone'} size={24} color="black" />
+            </View>
+            <View style={styles.sendIcon}>
+              <Icon name={'paper-plane'} size={20} color="white" />
+            </View>
+          </View>
+        </View>
+      </View>
     </View>
   );
 };
@@ -115,6 +138,44 @@ const styles = StyleSheet.create({
     fontSize: 22,
     fontWeight: 'bold',
     color: 'white',
+  },
+  chatConversationContainer: {
+    flex: 9,
+  },
+  sendMessageContainer: {
+    flex: 1,
+    backgroundColor: 'white',
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: 'black',
+    shadowOffset: {
+      width: 0,
+      height: 18,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 20.0,
+    elevation: 24,
+  },
+  customInputBox: {
+    backgroundColor: '#E5EBFD',
+    width: '90%',
+    height: '75%',
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  sendIcon: {
+    flex: 1,
+    margin: 5,
+    backgroundColor: '#3F7EFF',
+    width: 40,
+    height: 40,
+    borderRadius: 25,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
